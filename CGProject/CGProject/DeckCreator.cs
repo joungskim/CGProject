860182de83b1e46f56620f2610f686bb4d95a0e8
jkim <jkim@ksu.edu>
2014-12-04 03:00:33 -0600

    Created a Deck Creator Class

---
+++ CGProject/CGProject/DeckCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGProject
+{
+    public class DeckCreator
+    {
+        private List<Game> Games = new List<Game>();
+        private List<Card> Cards;
+
+
+
+        public void addCard(Card card)
+        {
+            Cards.Add(card);
+        }
+
+        public List<Game> getGames()
+        {
+            return Games;
+        }
+    }
+}
