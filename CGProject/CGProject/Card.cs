b37cd82da755738db4b070fecee76b8d5976e452
jkim <jkim@ksu.edu>
2014-12-04 01:04:24 -0600

    Created a Card Class

---
+++ CGProject/CGProject/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGProject
+{
+    class Card
+    {
+        private string Name;
+        private string Cost;
+        private string Value;
+        private string Description;
+        private string ImageName;
+
+        public Card(string Name, string Value, string Description, string ImageName, string Cost)
+        {
+            this.Name = Name;
+            this.Cost = Cost;
+            this.Value = Value;
+            this.Description = Description;
+            this.ImageName = ImageName;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getCost()
+        {
+            return Cost;
+        }
+
+        public string getValue()
+        {
+            return Value;
+        }
+
+        public string getDescription()
+        {
+            return ImageName;
+        }
+    }
+}
