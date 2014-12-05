77ce383df1d086a3ec3f15ad110a49dfff5707cb
jkim <jkim@ksu.edu>
2014-12-04 21:41:10 -0600

    Revert "Merge remote-tracking branch 'origin/Alex' into JRBranch1"
    This reverts commit 92b338238bc1adb2482ea0203c35256d08f01c16, reversing
    changes made to ea185ee22e0ddab34e791e0551d974e17d34431e.

--- CGProject/CGProject/Game.cs
+++ CGProject/CGProject/Game.cs
@@ -6,42 +6,8 @@ using System.Threading.Tasks;
 
 namespace CGProject
 {
-    /*
-     * Class that defines every game
-     * Manfact
-     * List of Cards 
-     * int GID
-     * 
-     * */
     public class Game
     {
-        private string GameName;
-        private string MadeBy;
-        private string GId;
-        private List<Card> Deck;
-        public Game(string gameName, string madeBy, string gID, List<Card> Deck)
-        {
-            this.GameName = gameName;
-            this.MadeBy = madeBy;
-            this.GId = gID;
-            this.Deck = Deck;
-        }
-        public string GetName()
-        {
-            return GameName;
-        }
-        public string GetMadeBy()
-        {
-            return MadeBy;
-        }
-        public string GetGId()
-        {
-            return GId;
-        }
-        public void AddList(List<Card> Deck)
-        {
-            this.Deck = Deck;
-        }
-       
+        private string gameName;
     }
 }
