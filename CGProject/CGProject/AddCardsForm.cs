d14645d084eaab89623b1f6572bdca5bfdd30181
aburch08 <aburch@ksu.edu>
2014-12-05 04:44:31 -0600

    File Parser

--- CGProject/CGProject/AddCardsForm.cs
+++ CGProject/CGProject/AddCardsForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CGProject
 {
@@ -15,6 +17,33 @@ namespace CGProject
         public AddCardsForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string line;
+            OpenFileDialog x = new OpenFileDialog();
+            DialogResult result = x.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(result.ToString());
+                try
+                {
+                    while(file.ReadLine()!= null)
+                    {
+
+                        int card_ID = 1;
+                        line = file.ReadLine();
+                        string[] values = line.Split(',');
+                        
+                    }
+                }
+                catch(IOException)
+                {
+
+                }
+            }
         }
     }
 }
