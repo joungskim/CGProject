098bd153b8df06daf08fd1bc8c5a648aab6b6cd7
aburch08 <aburch@ksu.edu>
2014-12-04 21:30:40 -0600

    Created Server Class

---
+++ CGProject/CGProject/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace CGProject
+{
+    public class Server
+    {
+        private string user;
+        private string pass;
+        public Server(string user, string pass)
+        {
+            this.user = user;
+            this.pass = pass;
+
+        }
+        public void MakeConnection()
+        {
+            try
+            {
+                string myConnection = "datasoure=localhost;port=3306;username=root;password=root";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand(" select * database.edata ;", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                myConn.Open();
+                DataSet ds = new DataSet();
+                MessageBox.Show(Success!);
+                myConn.close();
+
+            }
+            catch(Exeception ex)
+            {
+                MessageBox.Show(ex.message);
+            }
+
+        }
+        public void CloseConnection()
+        {
+            
+        }
+    }
+
+}
